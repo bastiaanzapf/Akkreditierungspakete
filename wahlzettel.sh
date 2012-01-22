acc1333479c17da11229dee7e3faf2faa83de800
Bastiaan Zapf <bzapf@online.de>
2012-01-22 15:53:47 +0100

    Umbenannt, Kommentare

--- wahlzettel.sh
+++ wahlzettel.sh
@@ -1,15 +1,23 @@
 
+# 18 verschiedene Stimmzettel
+
 for num in $(seq 1 18)
 do
 
-    pdflatex "\newcommand{\anumber}{$num}\input{wahlzettel.latex}"
+# Stimmzettel setzen
+
+    pdflatex "\newcommand{\anumber}{$num}\input{wahlzettel.latex}" 
+
+# jeweils 16 (4*4) davon in eine Datei
 
     pdfjoin wahlzettel.pdf wahlzettel.pdf wahlzettel.pdf wahlzettel.pdf \
 	wahlzettel.pdf wahlzettel.pdf wahlzettel.pdf wahlzettel.pdf \
 	wahlzettel.pdf wahlzettel.pdf wahlzettel.pdf wahlzettel.pdf \
 	wahlzettel.pdf wahlzettel.pdf wahlzettel.pdf wahlzettel.pdf \
-	--outfile seite.pdf
+	--outfile seite.pdf 
+
+# 16 Seiten zu einer zusammenfassen
     
-    pdfnup --paper a4 --nup 4x4 seite.pdf --outfile stimmzettel/$num.pdf
+    pdfnup --paper a4 --nup 4x4 seite.pdf --outfile wahlzettel/$num.pdf 
 
 done
